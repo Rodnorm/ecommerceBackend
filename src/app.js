eabbb6bc1564bb4562ab4e3563411c244304465b
rnormando <42142498+rnormando@users.noreply.github.com>
2018-11-15 11:48:50 -0200

    httpHeadersConfig
    * Also added limit size to json

--- src/app.js
+++ src/app.js
@@ -21,6 +21,15 @@ const order = require('./routes/order.route');
 //
 
 app.use(bodyParser.json());
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
+app.use((req, res, next) => {
+    res.header('Acces-Control-Allow-Origin', '*');
+    res.header('Acces-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept, x-access-token');
+    res.header('Acces-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', index);
 app.use('/produtos', produto);
