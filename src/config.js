af7948a72b7c983fed54851ff6f42e64da10cdd3
rnormando <42142498+rnormando@users.noreply.github.com>
2018-11-15 10:36:17 -0200

    removed config.js

--- src/config.js
+++
@@ -1,9 +0,0 @@
-global.SALT_KEY = 'ba501822-9e9d-4c6a-896b-0a81ab110506';
-global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo(a)!';
-
-module.exports = {
-    connectionString: 'mongodb://normando1:normando1@ds261247.mlab.com:61247/ecommerce-database',
-    //connectionEmail
-    sendgridKey: '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',
-    containerConnectionSring: 'TBD'
-}
